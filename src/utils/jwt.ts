797eaa1820d0ec43e7389155797ae6fde77b80ca
Zharckov <23assasyncred03@gmail.com>
2021-09-02 02:46:47 +0400

    Changes

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,10 +5,11 @@ export const gen = (data: string | object | Buffer) => jwt.sign(data, config.jwt
 
 export const check = (token: string) => jwt.verify(token, config.jwt_secret, { algorithms: ['HS256'] });
 
-export const getUserData = (token: string): { steamId: string; name: string; } => {
+export const getUserData = (token: string): { steamId: string; name: string; id: string } => {
     let data = JSON.parse(JSON.stringify(jwt.verify(token, config.jwt_secret, { algorithms: ['HS256'] })));
     return {
         steamId: data.steamId,
-        name: data.name
+        name: data.name,
+        id: data.id
     }
 }
